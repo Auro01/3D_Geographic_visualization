cee25b5a9f34cc33fc63551bc75813307f411be3
Eduardo Ancira Salazar <eanciras@gmail.com>
2018-05-06 14:21:34 -0500

    GLM Library + Mouse Movement example code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -20,158 +6,165 @@
 
 #include <stdlib.h>
 
-static int slices = 16;
-static int stacks = 16;
+#include <iostream>
+using namespace std;
 
-/* GLUT callback Handlers */
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
-static void resize(int width, int height)
+
+glm::dvec3 Unproject( const glm::dvec3& win )
 {
-    const float ar = (float) width / (float) height;
+    glm::ivec4 view;
+    glm::dmat4 proj, model;
+    glGetDoublev( GL_MODELVIEW_MATRIX, &model[0][0] );
+    glGetDoublev( GL_PROJECTION_MATRIX, &proj[0][0] );
+    glGetIntegerv( GL_VIEWPORT, &view[0] );
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
+    glm::dvec3 world = glm::unProject( win, model, proj, view );
+    return world;
+}
+
+// unprojects the given window point
+// and finds the ray intersection with the Z=0 plane
+glm::dvec2 PlaneUnproject( const glm::dvec2& win )
+{
+    glm::dvec3 world1 = Unproject( glm::dvec3( win, 0.01 ) );
+    glm::dvec3 world2 = Unproject( glm::dvec3( win, 0.99 ) );
+
+    // u is a value such that:
+    // 0 = world1.z + u * ( world2.z - world1.z )
+    double u = -world1.z / ( world2.z - world1.z );
+    // clamp u to reasonable values
+    if( u < 0 ) u = 0;
+    if( u > 1 ) u = 1;
+
+    return glm::dvec2( world1 + u * ( world2 - world1 ) );
+}
+
+// pixels per unit
+const double ppu = 1.0;
+
+glm::dvec2 center( 0 );
+double scale = 1.0;
+void ApplyCamera()
+{
+    glMatrixMode( GL_PROJECTION );
     glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    const double w = glutGet( GLUT_WINDOW_WIDTH ) / ppu;
+    const double h = glutGet( GLUT_WINDOW_HEIGHT ) / ppu;
+    glOrtho( -w/2, w/2, -h/2, h/2, -1, 1 );
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    glScaled( scale, scale, 1.0 );
+    glTranslated( -center[0], -center[1], 0 );
 }
 
-static void display(void)
+glm::dvec2 mPos;
+
+glm::dvec2 centerStart( 0 );
+int btn = -1;
+
+void mouse( int button, int state, int x, int y )
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+    ApplyCamera();
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
+    y = glutGet( GLUT_WINDOW_HEIGHT ) - y;
+    mPos = glm::ivec2( x, y );
 
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
-
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
+    btn = button;
+    if( GLUT_LEFT_BUTTON == btn && GLUT_DOWN == state )
     {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+        centerStart = PlaneUnproject( glm::dvec2( x, y ) );
+    }
+    if( GLUT_LEFT_BUTTON == btn && GLUT_UP == state )
+    {
+        btn = -1;
     }
 
     glutPostRedisplay();
 }
 
-static void idle(void)
+void motion( int x, int y )
 {
+    y = glutGet( GLUT_WINDOW_HEIGHT ) - y;
+    mPos = glm::ivec2( x, y );
+
+    if( GLUT_LEFT_BUTTON == btn )
+    {
+        ApplyCamera();
+        glm::dvec2 cur = PlaneUnproject( glm::dvec2( x, y ) );
+        center += ( centerStart - cur );
+    }
+
     glutPostRedisplay();
 }
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
+void passiveMotion( int x, int y )
 {
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    y = glutGet( GLUT_WINDOW_HEIGHT ) - y;
+    mPos = glm::ivec2( x, y );
+    glutPostRedisplay();
+}
 
-    glutCreateWindow("GLUT Shapes");
+void wheel( int wheel, int direction, int x, int y )
+{
+    y = glutGet( GLUT_WINDOW_HEIGHT ) - y;
+    mPos = glm::ivec2( x, y );
 
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
+    ApplyCamera();
+    glm::dvec2 beforeZoom = PlaneUnproject( glm::dvec2( x, y ) );
 
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    const double scaleFactor = 0.90;
+    if( direction == -1 )   scale *= scaleFactor;
+    if( direction ==  1 )   scale /= scaleFactor;
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
+    ApplyCamera();
+    glm::dvec2 afterZoom = PlaneUnproject( glm::dvec2( x, y ) );
 
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
+    center += ( beforeZoom - afterZoom );
 
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glutPostRedisplay();
+}
 
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+void display()
+{
+    glClearColor( 0, 0, 0, 1 );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    ApplyCamera();
+
+    glm::dvec2 cur = PlaneUnproject( mPos );
+    cout << cur.x << " " << cur.y << " " << scale << endl;
+
+    glPushMatrix();
+    glScalef( 50, 50, 1 );
+    glBegin( GL_QUADS );
+    glColor3ub( 255, 255, 255 );
+    glVertex2i( -1, -1 );
+    glVertex2i(  1, -1 );
+    glVertex2i(  1,  1 );
+    glVertex2i( -1,  1 );
+    glEnd();
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+int main( int argc, char **argv )
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE );
+    glutInitWindowSize( 600, 600 );
+    glutCreateWindow( "GLUT" );
+
+    glutMouseFunc( mouse );
+    glutMotionFunc( motion );
+    //glutMouseWheelFunc( wheel );
+    glutDisplayFunc( display );
+    glutPassiveMotionFunc( passiveMotion );
 
     glutMainLoop();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
